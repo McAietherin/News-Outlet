c7c03da8c32d7e332f5d97686b43e38b99c8481d
McAestro <mcaietherin@gmail.com>
2025-08-22 15:44:28 +0545

    second commit

--- src/pages/Highlights.jsx
+++ src/pages/Highlights.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Highlights() {
   return (
     <div>
-      if this works imma be mad dasfhkasdhfahsdk
+      Highlights
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, odio molestiae. Modi accusantium aliquam quo vero accusamus eveniet sint tenetur, impedit consequatur voluptatum corporis eaque fuga! Ipsa deleniti similique omnis.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, odio molestiae. Modi accusantium aliquam quo vero accusamus eveniet sint tenetur, impedit consequatur voluptatum corporis eaque fuga! Ipsa deleniti similique omnis.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit consectetur aliquam sunt, maiores impedit dolor repellat fugiat veritatis temporibus? Illo esse et molestiae. Voluptates tempore accusantium voluptas vero, magni doloribus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore deleniti, laudantium assumenda laboriosam impedit voluptatibus omnis praesentium architecto quam quaerat modi eos? Commodi quasi harum neque illo, laudantium temporibus?
+      </p>
     </div>
   )
 }
